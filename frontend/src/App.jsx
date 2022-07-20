d829a46f373b98e92e604baf1a8a9cfd79fecb46
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-07-20 11:52:53 +0200

    adding the pages and components front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import Home from "@pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
+import CustomCursor from "./components/CustomCursor";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    // create a routes to the different pages
+    <div className="">
+      <Router>
+        <CustomCursor />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SocialMedia from "./SocialMedia";
+
+function Footer() {
+  return (
+    <div className="footer flex justify-center">
+      <SocialMedia />
+    </div>
+  );
+}
+
+export default Footer;
