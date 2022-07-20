b5a3a4d0555cc3c7aa8b60721c0eadacf6de800a
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-07-20 16:16:53 +0200

    insert data in DB

--- backend/database.sql
+++ backend/database.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS `team` (
 ) ENGINE=InnoDB;
 
 INSERT INTO `team` (`id`, `name`, `quantity`) VALUES
-(1, 'Frontend', 5);
+(1, 'Frontend', 5),
+(2, 'Backend', 8);
 
 
 CREATE TABLE IF NOT EXISTS `dev` (
@@ -37,6 +38,12 @@ CREATE TABLE IF NOT EXISTS `team_dev` (
   FOREIGN KEY (`dev_id`) REFERENCES `dev` (`id`)
 ) ENGINE=InnoDB;
 
+INSERT INTO `team_dev` (`id`, `team_id`, `dev_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1),
+(4, 2, 2);
+
 
 CREATE TABLE IF NOT EXISTS `project` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -47,3 +54,7 @@ CREATE TABLE IF NOT EXISTS `project` (
   `team_id` int(11) NOT NULL,
   FOREIGN KEY (team_id) REFERENCES team(id)
 ) ENGINE=InnoDB;
+
+INSERT INTO `project` (`name`, `description`, `site_link`, `github_link`, `team_id`) VALUES
+('Mood', 'Mood is a project for the course "Programming in JS" at the University of Applied Sciences in the city of Stuttgart.', 'https://mood-une-application-de-choix.netlify.app/', 'https://github.com/randricalvin/Projet_2_React_Mood_WCS', 1),
+('Error404', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'https://kevinlavigne.github.io/projet1-error404/accueil.html', 'https://github.com/randricalvin/Pojet-1-Error404-WCS', 2);
